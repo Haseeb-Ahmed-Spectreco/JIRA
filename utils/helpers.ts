9379c932c3bcbd1569f9ec4e2d6050d826b1882e
Haseeb Ahmed <haseeb@spectreco.com>
2025-04-07 20:59:45 +0500

    URL VERCEL

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -9,7 +9,9 @@ export function getBaseUrl() {
   if (typeof window === "undefined") {
     return "";
   }
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url\
+  // if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  return `https://jira-rust.vercel.app`; // SSR should use vercel url\
+
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 }
 
